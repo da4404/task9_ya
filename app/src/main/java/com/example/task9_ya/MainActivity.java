30702442d3f8d0c5e8ca75429dd51e7c5bb50639
בר אופק <u9054555@bsmch.net>
2025-12-03 11:04:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/task9_ya/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.task9_ya;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu)
+    {
+
+        getMenuInflater () .inflate (R.menu.main,menu);
+
+        return super.onCreateOptionsMenu (menu);
+    }
+}
